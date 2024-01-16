840ddb36db8dd46e7e023ef8115aa7608990fe79
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-01-15 19:59:00 -0600

    commit

---
+++ AspCoreWebReact.Server/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspCoreWebReact.Server.Models;
+
+public partial class Employee
+{
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string City { get; set; } = null!;
+
+    public string Department { get; set; } = null!;
+
+    public string Gender { get; set; } = null!;
+}
